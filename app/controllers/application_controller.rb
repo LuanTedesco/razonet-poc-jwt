347bf1015800ad278beb5c3714a84dab156553ec
Luan Tedesco <luanfelipe2040@gmail.com>
2023-05-18 14:39:48 -0300

    rails credentials and time to exp token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::API
   before_action :authorized
 
   def encode_token(payload)
-    JWT.encode(payload, 'secret')
+    exp = Time.now.to_i + 4 * 3600
+    exp_payload = { data: 'data', exp: exp, user_id: payload[:user_id] }
+
+    JWT.encode(exp_payload, Rails.application.credentials.jwt_secret, 'HS256')
   end
 
   def auth_header
@@ -15,17 +18,17 @@ class ApplicationController < ActionController::API
     token = auth_header.split(' ')[1]
 
     begin
-      JWT.decode(token, 'secret', true, algorithm: 'HS256')
-    rescue Exeption => error
-      puts error
+      JWT.decode(token, Rails.application.credentials.jwt_secret, true, algorithm: 'HS256')
+    rescue StandardError => e
+      puts e
     end
   end
 
   def current_user
-    if decoded_token
-      user_id = decoded_token[0]['user_id']
-      @user = User.find_by(id: user_id)
-    end
+    return unless decoded_token && decoded_token[0]['exp'] > Time.now.to_i
+
+    user_id = decoded_token[0]['user_id']
+    @user = User.find_by(id: user_id)
   end
 
   def logged_in?
