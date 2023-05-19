680fb4696ec60783ea6422ed847d7a4f194f4c25
Luan Tedesco <luanfelipe2040@gmail.com>
2023-05-19 15:18:05 -0300

    added api_controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,30 +1,32 @@
-module Api::V1
-  class AuthController < ApplicationController
-    skip_before_action :authorized, only: [:create]
-  
-    def create
-      @user = User.find_by(username: user_login_params[:username])
-      # authenticate method comes from bcrypt
-      if @user && @user.authenticate(user_login_params[:password])
-        token = encode_token({ user_id: @user.id })
-        render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
-      else
-        render json: { message: 'Invalid username or password' }, status: :unauthorized
+module Api
+  module V1
+    class AuthController < ApiController
+      skip_before_action :authorized, only: [:create]
+
+      def create
+        @user = User.find_by(username: user_login_params[:username])
+        # authenticate method comes from bcrypt
+        if @user && @user.authenticate(user_login_params[:password])
+          token = encode_token({ user_id: @user.id })
+          render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+        else
+          render json: { message: 'Invalid username or password' }, status: :unauthorized
+        end
       end
-    end
-  
-    def destroy_session
-      return unless auth_header
-  
-      token = auth_header.split(' ')[1]
-      revoke_token(token)
-      render json: { message: 'Session Destroyed Successfully' }, status: :accepted
-    end
-  
-    private
-  
-    def user_login_params
-      params.permit(:username, :password, auth: {})
+
+      def destroy_session
+        return unless auth_header
+
+        token = auth_header.split(' ')[1]
+        revoke_token(token)
+        render json: { message: 'Session Destroyed Successfully' }, status: :accepted
+      end
+
+      private
+
+      def user_login_params
+        params.permit(:username, :password, auth: {})
+      end
     end
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,25 +1,27 @@
-module Api::V1
-  class UsersController < ApplicationController
-    skip_before_action :authorized, only: [:create]
-  
-    def profile
-      render json: { user: UserSerializer.new(current_user), revoked: is_revoked? }, status: :accepted
-    end
-  
-    def create
-      @user = User.create(user_params)
-      if @user.valid?
-        @token = encode_token(user_id: @user.id)
-        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
-      else
-        render json: { error: 'failed to create user' }, status: :not_acceptable
+module Api
+  module V1
+    class UsersController < ApiController
+      skip_before_action :authorized, only: [:create]
+
+      def profile
+        render json: { user: UserSerializer.new(current_user), revoked: is_revoked? }, status: :accepted
       end
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
+
+      def create
+        @user = User.create(user_params)
+        if @user.valid?
+          @token = encode_token(user_id: @user.id)
+          render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+          render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
     end
   end
 end
