c31ac2040cd4d32a8e4e4814fb7723df47cb5025
IgorFZ <igorf.zamp@gmail.com>
2023-05-22 17:12:16 -0300

    fiz revoke all tokens

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -50,22 +50,7 @@ module Api
     end
 
     def revoke_all_tokens
-      tokens = JwtTokenlist.where(user_id: decoded_token[0]['user_id'])
-      for token in tokens do
-        token.update(revoked: true)
-      end
-    end
-
-    def is_revoked?
-      return unless auth_header
-
-      decoded_token = JWT.decode(@token, Rails.application.credentials.jwt_secret, true, algorithm: 'HS256')
-      token = JwtTokenlist.where(jti: decoded_token[0]['jti'], user_id: decoded_token[0]['user_id'])
-      if token.empty?
-        return true
-      else
-        return false if token[0].revoked == false and token[0].exp > Time.now.to_i
-      end
+      JwtAllowlist.new.revoke_all(decoded_token[0]['user_id'])
     end
 
     private
