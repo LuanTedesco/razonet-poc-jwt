8e82234f9cc07dacaf88217aab9521d19c5444cd
IgorFZ <igorf.zamp@gmail.com>
2023-05-23 15:51:37 -0300

    feat: add enum role to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: { case_sensitive: false }
+
+  enum :role, user: "user", admin: "admin", developer: "developer", marketing: "marketing"
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+  
+  def admin?
+    self.role == "admin"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_201321) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_175823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "role", ["user", "admin", "developer", "marketing"]
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -20,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_201321) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.enum "role", default: "user", null: false, enum_type: "role"
   end
 
 end
