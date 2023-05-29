211b189ea58aeeb30bdc08292d6bf682d3e6af64
Luan Tedesco <luanfelipe2040@gmail.com>
2023-05-29 16:39:45 -0300

    added column phone to table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_175823) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_172905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_175823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.enum "role", default: "user", null: false, enum_type: "role"
+    t.string "ddi_phone", limit: 3, default: "55", null: false
+    t.string "ddd_phone", limit: 2, null: false
+    t.string "phone", limit: 9, null: false
   end
 
 end
